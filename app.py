e2e427e0399d933bb8a272c10b1cc044f73e28c9
Tiago F. Hintehrolz <fh.tiago@gmail.com>
2025-02-10 22:59:27 -0300

    Adicionada rota de campos e insights das contas

--- app.py
+++ app.py
@@ -50,5 +50,50 @@ def get_accounts(platform):
     
     return jsonify({'error': f'Falha no acesso das contas da plataforma {platform}'}), response.status_code
 
+@app.route('/fields/<platform>', methods = ['GET'])
+def get_fields(platform):
+    
+    platform_mapping = {
+        "facebook": "meta_ads",
+        "google": "ga4",
+        "tiktok": "tiktok_insights"
+    }
+    platform_value = platform_mapping.get(platform.lower()) # só garantir a normalização da entrada pra evitar erros
+    
+    if not platform_value:
+        return jsonify({"error": "Plataforma inválida. Use: facebook, google ou tiktok."}), 400
+    
+    page = request.args.get("page", 1)
+    
+    response = requests.get(f'{BASE_URL}/fields?platform={platform_value}&page={page}', headers = HEADERS)
+    
+    if response.status_code == 200:
+        response.encoding = 'utf-8'
+        return jsonify(response.json())
+
+    return jsonify({'error': f'Falha ao obter campos para {platform}'}), response.status_code
+
+@app.route('/insights', methods = ['GET'])
+def get_insights():
+    
+    platform = request.args.get('platform')
+    account = request.args.get('account')
+    token = request.args.get('token')
+    fields = request.args.get('fields')
+    
+    if not platform or not account or not token or not fields:
+        return jsonify({'error':'Parâmetros necessários para acessar as rotas: platform, accout, toke e fields'})
+    
+    response = requests.get(
+        f'{BASE_URL}/insights?platform={platform}&account={account}&token={token}&fields={fields}', 
+        headers = HEADERS
+    )
+    
+    if response.status_code == 200:
+        response.encoding = 'utf-8'
+        return jsonify(response.json())
+
+    return jsonify({'error': f'Falha ao acessar os insights da conta {account}'})
+
 if __name__ == '__main__':
     app.run(debug = True)
